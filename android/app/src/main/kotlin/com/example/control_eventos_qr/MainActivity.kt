a31ee85bb286f6b02b9f745858f2a67b4f5ad743
Alvardud <alvaro_dudutex@outlook.es>
2019-10-02 11:22:17 -0400

    init commit

---
+++ android/app/src/main/kotlin/com/example/control_eventos_qr/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.control_eventos_qr
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
